f15501faf6c94204c362c2ea4dbc37d35c9dcff6
Dafna Libman <dafnalibman@gmail.com>
2020-01-15 15:54:43 +0000

    Add 3 methods to print out the header, student list and footer

--- directory.rb
+++ directory.rb
@@ -12,10 +12,18 @@ students = [
 "Joffrey Baratheon",
 "Norman Bates",
 ]
-# and then print them
-puts "The students of Villains Academy"
-"-------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end 
+def print(names)
+  names.each do |name|
+  puts name
+  end
 end
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+print_header
+print(students)
+print_footer(students)
